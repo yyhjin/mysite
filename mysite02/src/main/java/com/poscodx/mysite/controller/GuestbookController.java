d8e6797a56b0077ff310561c0389b4d83a9ad5cd
yyhjin <wendy0301@naver.com>
2023-09-07 17:24:39 +0900

    mysite - guestbook

--- mysite02/src/main/java/com/poscodx/mysite/controller/GuestbookController.java
+++ mysite02/src/main/java/com/poscodx/mysite/controller/GuestbookController.java
@@ -1,10 +1,16 @@
 package com.poscodx.mysite.controller;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.poscodx.mysite.dao.GuestbookDao;
+import com.poscodx.mysite.vo.GuestbookVo;
 
 public class GuestbookController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -12,9 +18,48 @@ public class GuestbookController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		
-		request
-			.getRequestDispatcher("/WEB-INF/views/guestbook/index.jsp")
-			.forward(request, response);
+		String action = request.getParameter("a");
+		
+		if("insert".equals(action)) {			
+			String name = request.getParameter("name");
+			String password = request.getParameter("password");
+			String content = request.getParameter("content");
+			
+			GuestbookVo vo = new GuestbookVo();
+			vo.setName(name);
+			vo.setPassword(password);
+			vo.setContents(content);
+			
+			new GuestbookDao().insert(vo);
+
+			response.sendRedirect("/mysite02/guestbook");
+			
+		} else if("deleteform".equals(action)) {
+			String no = request.getParameter("no");
+			
+			request.setAttribute("no", no);
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/guestbook/deleteform.jsp");
+			rd.forward(request, response);
+		} else if("delete".equals(action)) {
+			String no = request.getParameter("no");
+			String password = request.getParameter("password");
+			
+			if(password.equals(new GuestbookDao().findPasswordByNo(Integer.parseInt(no)))) {
+				new GuestbookDao().deleteByNo(no);	
+			}
+			
+			response.sendRedirect("/mysite02/guestbook");
+		} else {
+			List<GuestbookVo> list = new GuestbookDao().findAll();
+			
+			request.setAttribute("list", list);
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/guestbook/index.jsp");
+			rd.forward(request, response);
+		}
+		
+//		request
+//			.getRequestDispatcher("/WEB-INF/views/guestbook/index.jsp")
+//			.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- mysite02/src/main/webapp/WEB-INF/views/guestbook/index.jsp
+++ mysite02/src/main/webapp/WEB-INF/views/guestbook/index.jsp
@@ -1,4 +1,10 @@
+<%@page import="com.poscodx.mysite.vo.GuestbookVo"%>
+<%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%
+	List<GuestbookVo> list = (List<GuestbookVo>)request.getAttribute("list");
+	int len = list.size();
+%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,24 +14,15 @@
 </head>
 <body>
 	<div id="container">
-		<div id="header">
-			<h1>MySite</h1>
-			<ul>
-				<li><a href="">로그인</a><li>
-				<li><a href="">회원가입</a><li>
-				<li><a href="">회원정보수정</a><li>
-				<li><a href="">로그아웃</a><li>
-				<li>님 안녕하세요 ^^;</li>
-			</ul>
-		</div>
+		<jsp:include page="/WEB-INF/views/includes/header.jsp"/>
 		<div id="content">
 			<div id="guestbook">
-				<form action="/guestbook" method="post">
+				<form action="<%=request.getContextPath()%>/guestbook" method="post">
 					<input type="hidden" name="a" value="insert">
 					<table>
 						<tr>
 							<td>이름</td><td><input type="text" name="name"></td>
-							<td>비밀번호</td><td><input type="password" name="pass"></td>
+							<td>비밀번호</td><td><input type="password" name="password"></td>
 						</tr>
 						<tr>
 							<td colspan=4><textarea name="content" id="content"></textarea></td>
@@ -38,34 +35,31 @@
 				<ul>
 					<li>
 						<table>
+						<%
+							for(GuestbookVo vo : list) {
+						%>
 							<tr>
-								<td>[4]</td>
-								<td>안대혁</td>
-								<td>2015-11-10 11:22:30</td>
-								<td><a href="">삭제</a></td>
+								<td>[<%=len-- %>]</td>
+								<td><%=vo.getName() %></td>
+								<td><%=vo.getRegDate() %></td>
+								<td><a href="<%=request.getContextPath()%>/guestbook?a=deleteform&no=<%=vo.getNo() %>">삭제</a></td>
 							</tr>
 							<tr>
 								<td colspan=4>
-								안녕하세요. ^^;<br>
-								하하하하	
+								<%=vo.getContents().replaceAll("\n", "<br>") %>
 								</td>
 							</tr>
+						<%
+							}
+						%>
 						</table>
 						<br>
 					</li>
 				</ul>
 			</div>
 		</div>
-		<div id="navigation">
-			<ul>
-				<li><a href="">안대혁</a></li>
-				<li><a href="">방명록</a></li>
-				<li><a href="">게시판</a></li>
-			</ul>
-		</div>
-		<div id="footer">
-			<p>(c)opyright 2015, 2016, 2017, 2018</p>
-		</div>
+		<jsp:include page="/WEB-INF/views/includes/navigation.jsp" />
+		<jsp:include page="/WEB-INF/views/includes/footer.jsp" />
 	</div>
 </body>
 </html>
