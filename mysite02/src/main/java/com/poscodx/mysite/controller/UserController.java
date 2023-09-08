0a895e4f77fa657366213bf6387304dbe96270f6
yyhjin <wendy0301@naver.com>
2023-09-08 09:55:10 +0900

    mysite user-join #1

--- mysite02/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.poscodx.mysite.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.poscodx.mysite.dao.UserDao;
+import com.poscodx.mysite.vo.UserVo;
 
 public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -18,7 +22,20 @@ public class UserController extends HttpServlet {
 			request
 				.getRequestDispatcher("/WEB-INF/views/user/joinform.jsp")
 				.forward(request, response);;
-		} 
+		} else if("join".equals(action)) {
+			String name = request.getParameter("name");
+			String email = request.getParameter("email");
+			String password = request.getParameter("password");
+			String gender = request.getParameter("gender");
+			
+			UserVo userVo = new UserVo();
+			userVo.setName(name);
+			userVo.setEmail(email);
+			userVo.setPassword(password);
+			userVo.setGender(gender);
+			
+			new UserDao().insert(userVo);
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ mysite02/src/main/java/com/poscodx/mysite/dao/UserDao.java
@@ -0,0 +1,203 @@
+package com.poscodx.mysite.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.poscodx.mysite.vo.UserVo;
+
+public class UserDao {
+
+	public List<UserVo> findAll() {
+		List<UserVo> result = new ArrayList<UserVo>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			
+			String sql =
+					"select no, name, contents, reg_date "
+					+ "from guestbook "
+					+ "order by reg_date desc";
+			pstmt = conn.prepareStatement(sql);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				int no = rs.getInt(1);
+				String name = rs.getString(2);
+				String contents = rs.getString(3);
+				String regDate = rs.getString(4);
+				
+				UserVo vo = new UserVo();
+				vo.setName(name);
+				
+				result.add(vo);
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("Guestbook Select error: " + e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+
+	public String findPasswordByNo(int no) {
+		String result = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			
+			String sql =
+					"select password "
+					+ "from guestbook "
+					+ "where no = ?";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setInt(1, no);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				String password = rs.getString(1);
+				
+				result = password;
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("Guestbook Select error: " + e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+	
+	public boolean insert(UserVo vo) {
+		boolean result = false;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = getConnection();
+			
+			String sql =
+					"insert into "
+					+ "values(null, ?, ?, password(?), ?, current_date())";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getEmail());
+			pstmt.setString(3, vo.getPassword());
+			pstmt.setString(4, vo.getGender());
+			
+			int cnt = pstmt.executeUpdate();
+			
+			result = cnt == 1;
+			
+		} catch (SQLException e) {
+			System.out.println("User Insert error: " + e);
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+
+	public void deleteByNo(String no) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			
+			String sql =
+					"delete from guestbook "
+					+ "where no = ?";
+			pstmt = conn.prepareStatement(sql);
+				
+			pstmt.setString(1, no);
+			
+			rs = pstmt.executeQuery();
+			
+		} catch (SQLException e) {
+			System.out.println("Guestbook Delete error: " + e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+			
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mariadb://192.168.0.175:3307/webdb?charset=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "webdb123");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패 : " + e);
+		} 
+
+		return conn;
+	}
+	
+}
