0cfd21acbdff6606bf82bb0cea00c6dad7c294ca
yyhjin <wendy0301@naver.com>
2023-09-18 09:48:36 +0900

    user #1

---
+++ mysite03/src/main/java/com/poscodx/mysite/repository/UserRepository.java
@@ -0,0 +1,247 @@
+package com.poscodx.mysite.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.stereotype.Repository;
+
+import com.poscodx.mysite.vo.UserVo;
+
+@Repository
+public class UserRepository {
+
+	public UserVo findByEmailAndPassword(String email, String password) {
+		UserVo userVo = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			
+			String sql =
+					"select no, name "
+					+ "from user "
+					+ "where email = ? "
+					+ "and password = password(?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, email);
+			pstmt.setString(2, password);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				Long no = rs.getLong(1);
+				String name = rs.getString(2);
+				
+				userVo = new UserVo();
+				userVo.setNo(no);
+				userVo.setName(name);
+				userVo.setEmail(email);
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("User Insert error: " + e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return userVo;
+	}
+	
+	public UserVo findByNo(long no) {
+		UserVo userVo = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			
+			String sql =
+					"select email, gender, name "
+					+ "from user "
+					+ "where no = ? ";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setLong(1, no);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				String email = rs.getString(1);
+				String gender = rs.getString(2);
+				String name = rs.getString(3);
+				
+				userVo = new UserVo();
+				userVo.setEmail(email);
+				userVo.setGender(gender);
+				userVo.setName(name);
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("User Select error: " + e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return userVo;
+	}
+	
+	public boolean insert(UserVo vo) {
+		boolean result = false;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = getConnection();
+			
+			String sql =
+					"insert into user "
+					+ "values(null, ?, ?, password(?), ?, current_date())";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getEmail());
+			pstmt.setString(3, vo.getPassword());
+			pstmt.setString(4, vo.getGender());
+			
+			int cnt = pstmt.executeUpdate();
+			
+			result = cnt == 1;
+			
+		} catch (SQLException e) {
+			System.out.println("User Insert error: " + e);
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+	
+	public boolean update(UserVo vo) {
+		boolean result = false;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = getConnection();
+			
+			String sql =
+					"update user "
+					+ "set name=?, password=password(?), gender=? "
+					+ "where no=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getPassword());
+			pstmt.setString(3, vo.getGender());
+			pstmt.setLong(4, vo.getNo());
+			
+			int cnt = pstmt.executeUpdate();
+			
+			result = cnt == 1;
+			
+		} catch (SQLException e) {
+			System.out.println("User Update error: " + e);
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+
+	public void deleteByNo(String no) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			
+			String sql =
+					"delete from guestbook "
+					+ "where no = ?";
+			pstmt = conn.prepareStatement(sql);
+				
+			pstmt.setString(1, no);
+			
+			rs = pstmt.executeQuery();
+			
+		} catch (SQLException e) {
+			System.out.println("Guestbook Delete error: " + e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+			
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mariadb://192.168.0.175:3307/webdb?charset=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "webdb123");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패 : " + e);
+		} 
+
+		return conn;
+	}
+	
+}
--- mysite03/src/main/webapp/WEB-INF/views/user/join.jsp
+++ mysite03/src/main/webapp/WEB-INF/views/user/join.jsp
@@ -16,20 +16,21 @@
 		<div id="content">
 			<div id="user">
 
-				<form id="join-form" name="joinForm" method="post" action="${pageContext.request.contextPath}/user?a=join">
-					<input type="hidden" name="a" value="join">
-					<label class="block-label" for="name">이름</label> <input id="name"
-						name="name" type="text" value=""> <label
-						class="block-label" for="email">이메일</label> <input id="email"
-						name="email" type="text" value=""> <input type="button"
-						value="id 중복체크"> <label class="block-label">패스워드</label> <input
-						name="password" type="password" value="">
+				<form id="join-form" name="joinForm" method="post" action="${pageContext.request.contextPath}/user/join">
+					<label class="block-label" for="name">이름</label> 
+					<input id="name" name="name" type="text" value=""> 
+					
+					<label class="block-label" for="email">이메일</label> 
+					<input id="email" name="email" type="text" value=""> 
+					<input type="button" value="id 중복체크">
+					
+					<label class="block-label">패스워드</label> 
+					<input name="password" type="password" value="">
 
 					<fieldset>
 						<legend>성별</legend>
-						<label>여</label> <input type="radio" name="gender" value="female"
-							checked="checked"> <label>남</label> <input type="radio"
-							name="gender" value="male">
+						<label>여</label> <input type="radio" name="gender" value="female" checked="checked"> 
+						<label>남</label> <input type="radio" name="gender" value="male">
 					</fieldset>
 
 					<fieldset>
