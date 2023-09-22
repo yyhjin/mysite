85809998fa2298cd165c0e39c158b7b1d966c103
yyhjin <wendy0301@naver.com>
2023-09-22 11:53:27 +0900

    Validation #2

--- mysite03/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.poscodx.mysite.controller;
 
-import javax.servlet.http.HttpSession;
+import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,7 +30,18 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/join", method=RequestMethod.POST)
-	public String join(@ModelAttribute UserVo userVo) {
+	public String join(@Valid UserVo userVo, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+//			List<ObjectError> list = result.getAllErrors();
+//			for(ObjectError error: list) {
+//				System.out.println(error);
+//			}
+			
+			model.addAllAttributes(result.getModel());
+			model.addAttribute("userVo", userVo);
+			return "user/join";
+		}
+		
 		userService.addUser(userVo);
 		return "redirect:/user/joinsuccess";
 	}
